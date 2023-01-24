3b3d56479b71457a55ff63256816e354b27c3e87
R0GU3-0N3 <122492020+R0GU3-0N3@users.noreply.github.com>
2023-01-24 14:15:49 -0700

    Added Unit1aLabpart2.cs

---
+++ Unit1a/Unit1aLabpart2.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Variables
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      string name = "Jimmy";
+	  int myAge = 15;
+	  myAge = 16; //Jimmy's number is now 16
+	  Console.WriteLine(name);
+	  Console.WriteLine(myAge);
+    }
+  }
+}
